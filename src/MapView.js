3ec357a6a32cc4430f070f5073013e3d66f1a758
Nikhil Wadekar <nwadekar00@langara.ca>
2019-10-01 13:17:28 -0700

    Created MapView

---
+++ src/MapView.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./MapView.css";
+
+function MapView() {
+  return (
+    <div className="MapView ">
+      
+    </div>
+  );
+}
+
+export default MapView;
