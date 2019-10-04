2d3a3c37232532d10d93904d151ad3ec96d281f0
Sam Thandi <sonuthandi85@gmail.com>
2019-10-04 10:41:32 -0700

    Created Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./Hero";
+// import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <Hero />
+    </div>
+  );
+}
+
+export default Footer;
