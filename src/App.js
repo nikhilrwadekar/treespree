ae1d164a0ea9f6590e4acff8291043a0aebd0450
jasminekaur95 <kjasmine2810@gmail.com>
2019-10-20 21:20:21 -0700

    added header and grid view components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Hero from "./Hero";
+import Header from "./Header";
+import Gridview from "./Gridview";
 import "./App.css";
 import WrappedMap from "./MapView";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Hero />
+      <Gridview />
       <div style={{ width: "100vw", height: "100vh" }}>
-        <WrappedMap
+      <WrappedMap
           // Add &key=API_KEY when you get one to get out of DEV mode.
           isMarkerShown={false}
           googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
@@ -16,6 +20,10 @@ function App() {
           containerElement={<div style={{ height: "100%" }} />}
           mapElement={<div style={{ height: "100%" }} />}
         />
+
+
+        
+
       </div>
     </div>
   );
