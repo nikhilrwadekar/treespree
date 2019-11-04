923aae244d71548dd912c18cdb4b08a2165ce26d
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-11-03 16:40:33 -0800

    Added Total Tree Count for Neighbourhoods

--- controllers/neighbourhoodController.js
+++ controllers/neighbourhoodController.js
@@ -5,7 +5,15 @@ const { cp } = require("../db/connection");
 // GET '/api/neighbourhoods'
 exports.getNeighbourhoods = (req, res) => {
   // Send all Tree Types
-  query(cp, `SELECT * from neighbourhoods`)
+  query(
+    cp,
+    `SELECT neighbourhoods.*, count(trees.neighbourhood_id) as neighbourhood_tree_count        
+    from neighbourhoods
+    left join trees
+    on (neighbourhoods.neighbourhood_id = trees.neighbourhood_id)
+    group by
+        neighbourhoods.neighbourhood_id`
+  )
     .then(results => res.send(results))
     .catch(error => res.send(error));
 };
