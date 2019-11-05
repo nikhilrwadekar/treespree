39bb3ef987c68f20197c190d2bd8b71facfd1ee0
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-11-05 13:02:30 -0800

    Change #22

--- controllers/neighbourhoodController.js
+++ controllers/neighbourhoodController.js
@@ -7,7 +7,7 @@ exports.getNeighbourhoods = (req, res) => {
   // Send all Tree Types
   query(
     cp,
-    `SELECT neighbourhoods.*, count(trees.neighbourhood_id) as neighbourhood_tree_count        
+    `SELECT neighbourhoods.*, count(trees.neighbourhood_id) as neighbourhood_tree_count
     from neighbourhoods
     left join trees
     on (neighbourhoods.neighbourhood_id = trees.neighbourhood_id)
