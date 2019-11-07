a98044dad89d4cc2cad42614557374bcc9208479
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-11-07 13:17:03 -0800

    Added Abs Name & Common Count to /api/trees

--- controllers/treeController.js
+++ controllers/treeController.js
@@ -9,13 +9,17 @@ exports.getTrees = (req, res) => {
   // Send all Trees - limited to 10 now
   query(
     cp,
-    `SELECT trees.tree_id, trees.tree_planted, trees.tree_diameter, trees.tree_latitude, trees.tree_longitude, neighbourhoods.neighbourhood_name, genus.genus_name, species.species_name, common_names.common_name_tree  
+    `SELECT trees.tree_id, trees.tree_planted, trees.tree_diameter, trees.tree_latitude, trees.tree_longitude,
+    neighbourhoods.neighbourhood_name, genus.genus_name, species.species_name, common_names.common_name_tree,
+    (SELECT COUNT(*) FROM trees WHERE trees.common_name_id = common_names.common_name_id) as common_name_tree_count,
+    absolute_common_names.absolute_common_name_tree absolute_common_name
   FROM trees
   INNER JOIN neighbourhoods ON trees.neighbourhood_id = neighbourhoods.neighbourhood_id
   INNER JOIN genus ON trees.genus_id = genus.genus_id
-  INNER JOIN species ON trees.neighbourhood_id = neighbourhoods.neighbourhood_id
-  INNER JOIN common_names ON trees.neighbourhood_id = neighbourhoods.neighbourhood_id
-  LIMIT 10;`
+  INNER JOIN species ON trees.species_id = species.species_id
+  INNER JOIN common_names ON trees.common_name_id = common_names.common_name_id
+  INNER JOIN absolute_common_names ON common_names.absolute_common_name_id = absolute_common_names.absolute_common_name_id
+  LIMIT 1000;`
   )
     .then(results => res.send(results))
     .catch(error => res.send(error));
