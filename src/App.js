f9787c2d323898b28ce49b94a27c374df28cafeb
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-11-08 02:13:00 -0800

    Added Functionality for Grid View & Map View; Fixed Homepage

--- src/App.js
+++ src/App.js
@@ -10,12 +10,10 @@ import Header from "./Header";
 import "./App.css";
 import Single from "./Single";
 import Contact from "./Contact";
-import Gridview from "./Gridview";
 import Terms from "./Terms";
 import Team from "./teamPage";
 import Footer from "./Footer";
-import WrappedMap from "./MapView";
-import GridViewV2 from "./GridViewV2";
+import GridMapView from "./GridMapView";
 import Graphview from "./Graphview";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -38,28 +36,26 @@ class App extends React.Component {
           <Route exact path="/">
             <Hero />
             <Graphview />
-            <GridViewV2 />
-            {/* Pagination Goes Here */}
-            <div style={{ width: "100vw", height: "100vh" }}>
-              <WrappedMap
-                // Add &key=API_KEY when you get one to get out of DEV mode.
-                isMarkerShown={false}
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
-                loadingElement={<div style={{ height: "100%" }} />}
-                containerElement={<div style={{ height: "100%" }} />}
-                mapElement={<div style={{ height: "100%" }} />}
-              />
-            </div>
+            {/* Newsletter Component Here */}
           </Route>
-          <Route exact path="/single">
-            <Single />
+
+          <Route exact path="/explore">
+            <GridMapView />
           </Route>
+
+          {/* Single Component accessed with Tree ID (Sent from Map View) */}
+          <Route exact path="/tree/id/:tree_id" component={Single} />
+          {/* Single Component accessed with Tree Name (Sent from Map Grid View) */}
+          <Route exact path="/tree/name/:tree_name" component={Single} />
+
           <Route exact path="/contact">
             <Contact />
           </Route>
-          <Route exact path="/termsAndCondition">
+
+          <Route exact path="/terms">
             <Terms />
           </Route>
+
           <Route exact path="/team">
             <Team />
           </Route>
---
+++ src/GridMapView.js
@@ -0,0 +1,51 @@
+import React from "react";
+import GridViewV2 from "./GridViewV2";
+import WrappedMap from "./MapView";
+
+class GridMapView extends React.Component {
+  state = {
+    gridViewIsActive: true,
+    mapViewIsActive: false
+  };
+
+  // This 'lifecycle hook' or function will run BEFORE the component is rendered. Pretty useful to set data from external API into the state.
+  componentWillMount() {}
+
+  // If the component renders..
+  componentDidMount() {}
+
+  toggleMapGrid() {
+    this.setState({
+      gridViewIsActive: !this.state.gridViewIsActive,
+      mapViewIsActive: !this.state.mapViewIsActive
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {/* Component Toggler STARTS */}
+        <button onClick={this.toggleMapGrid.bind(this)}>TOGGLE</button>
+        {/* Component Toggler ENDS */}
+
+        {this.state.gridViewIsActive && <GridViewV2 />}
+        {/* Map View Starts */}
+        {this.state.mapViewIsActive && (
+          <div style={{ width: "100%", height: "800px" }}>
+            <WrappedMap
+              // Add &key=API_KEY when you get one to get out of DEV mode.
+              isMarkerShown={false}
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+              loadingElement={<div style={{ height: "100%" }} />}
+              containerElement={<div style={{ height: "100%" }} />}
+              mapElement={<div style={{ height: "100%" }} />}
+            />
+          </div>
+        )}
+        {/* Map View Ends */}
+      </>
+    );
+  }
+}
+
+export default GridMapView;
