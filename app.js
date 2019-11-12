c859b1132478d967d01dce21bff3f59072930665
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-11-12 13:39:20 -0800

    Fixed routing for React on Express Server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 // CORS for local development and remote API requests
 var cors = require("cors");
@@ -12,12 +13,17 @@ app.listen(8080, () => {
   console.log("Listening on port 8080!");
 });
 
-// Static Middleware to serve the public folder as is
-app.use(express.static("build"));
-
-// Use JSON - This will allow you to work with req.body
-app.use(express.json());
-
 // Use Router
 const router = require("./routes/index.js");
 app.use("/api", router);
+
+// Static Middleware to serve the public folder as is
+app.use(express.static(path.join(__dirname, "build")));
+
+// Route handler for anything other than the API (Resolves to React router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+// Use JSON - This will allow you to work with req.body
+app.use(express.json());
