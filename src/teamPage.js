2eb652da407f9192cae50ebca54b89216a5ae573
Blandy Castro <blandycastro@Blandys-MacBook-Air.local>
2019-11-15 15:35:21 -0800

    V2 css team page

--- src/teamPage.js
+++ src/teamPage.js
@@ -6,38 +6,52 @@ state={
   teamMembers: [
     {
       name: "Jasmine Kaur",
-      Description: "Project Manager",
+      Description: "Project Manager | Front-end Developer",
       img: "/images/team-photos/Jasmine.jpg",
+      github:"/images/team-photos/github-logo.png",
+      linkdin:"/images/team-photos/linkdin-logo.png",
     },
     {
       name: "Blandy Castro",
       Description: "Back-end Developer",
       img: "/images/team-photos/Blandy.jpg",
+      github:"/images/team-photos/github-logo.png",
+      linkdin:"/images/team-photos/linkdin-logo.png",
     },
     {
       name: "Satnam Thandi",
       Description: "Back-end Developer",
       img: "/images/team-photos/Sam.jpg",
+      github:"/images/team-photos/github-logo.png",
+      linkdin:"/images/team-photos/linkdin-logo.png",
     } ,
     {
       name: "Darshpreet Kaur",
-      Description: "Designer",
+      Description: "UI-Designer",
       img: "/images/team-photos/Darshpreet.jpg",
+      github:"/images/team-photos/github-logo.png",
+      linkdin:"/images/team-photos/linkdin-logo.png",
     } ,
     {
       name: "Angel Augustine",
-      Description: "QA",
+      Description: "QA | Front-end Developer",
       img: "/images/team-photos/Angel.jpg",
+      github:"/images/team-photos/github-logo.png",
+      linkdin:"/images/team-photos/linkdin-logo.png",
     },
     {
       name: "Davinder Dhindsa",
-      Description: "Designer",
+      Description: "UX-Designer",
       img: "/images/team-photos/Davinder.jpg",
+      github:"/images/team-photos/github-logo.png",
+      linkdin:"/images/team-photos/linkdin-logo.png",
     },
     {
       name: "Nikhil Wadekar",
-      Description: "Front-end Developer",
+      Description: "Full-stock Developer",
       img: "/images/team-photos/Nikhil.jpg",
+      github:"/images/team-photos/github-logo.png",
+      linkdin:"/images/team-photos/linkdin-logo.png",
     }
   ]
 }
@@ -48,7 +62,7 @@ createMember = () => {
   for (let i = 0; i < 7; i++) {
 
     let children=[]
-    children.push(<img src={this.state.teamMembers[i].img} alt="Jazmin" className="img_teamPage" />)
+    children.push(<img src={this.state.teamMembers[i].img} alt={this.state.teamMembers[i].name} className="img_teamPage" />)
   
     let children1 = []
       children1.push(this.state.teamMembers[i].name)
@@ -56,9 +70,17 @@ createMember = () => {
     let children2 = []
       children2.push(this.state.teamMembers[i].Description)
 
-      member.push(<div className="img-holder">{children}
-      <p>{children1}</p>
-      <p>{children2}</p></div>)
+    let github =[]
+    github.push(<img src={this.state.teamMembers[i].github} alt="github" className="img_git" />)
+    
+    let linkdin =[]
+    linkdin.push(<img src={this.state.teamMembers[i].linkdin} alt="linkdin" className="img_link" />)
+  
+    member.push(<div className="team-holder"><div className="img-holder">{children}</div>
+   <div className="txt-holder"> <h5>{children1}</h5>
+    <p>{children2}</p>
+    {github}
+    {linkdin}</div></div>)
   }
   return member
 }
