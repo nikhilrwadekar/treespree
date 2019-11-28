442c6b431171a97c6fcd9758e08642eab2d2801a
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-11-28 07:45:07 -0800

    Latest FIXED Build -- 28-11 07:45 AM

--- build/precache-manifest.43a2d2286e73d8a3c6a9d976e28a407e.js
+++ build/precache-manifest.43a2d2286e73d8a3c6a9d976e28a407e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9dac56bdd13c94cdd831049cf2478666",
+    "revision": "73e8b19e3d8f8f82b3894462cc551d7b",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.56e9ed99.chunk.css"
   },
   {
-    "revision": "15eb8c116b41b26f9ff5",
+    "revision": "53e54273e5796144e5ec",
     "url": "/static/css/main.eda21b5b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9609a299.chunk.js"
   },
   {
-    "revision": "15eb8c116b41b26f9ff5",
-    "url": "/static/js/main.b0a00f73.chunk.js"
+    "revision": "53e54273e5796144e5ec",
+    "url": "/static/js/main.e92d05d8.chunk.js"
   },
   {
     "revision": "d49a6b0602bb2f1e88ef",
