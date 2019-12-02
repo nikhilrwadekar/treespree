b26d1c2898a141961e6f8a4d383a7520fa851d5d
jasminekaur95 <kjasmine2810@gmail.com>
2019-12-02 02:38:00 -0800

    addded grid hero section and its css

---
+++ src/GridHeroSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./GridHeroSection.css";
+
+function GridHeroSection() {
+  return (
+    <>
+      <div className="grid-hero">
+        <img src="/images/grid-image/grid-image-3.jpg" alt="" />
+        <div className="contentOverlay">
+          <h2>Search trees and know more about them!</h2>
+          <p>
+            Get extensive knowledge of trees using leaf illustrations. Enhance
+            your experience with our advanced filters.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GridHeroSection;
