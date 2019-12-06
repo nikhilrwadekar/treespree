3c4dd0c063a034f781162fee01c7abc8dea602df
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-12-06 00:53:15 -0800

    Fixed UX for Neighbourhood Selection

--- src/Graphview.js
+++ src/Graphview.js
@@ -5,14 +5,74 @@ import SelectOption from "./SelectOption";
 
 class Graphview extends Component {
   state = {
-    selectedNeighbourhoods: []
+    selectedNeighbourhoods: [],
+    selectionStatement: (
+      <p>
+        <b>Pick your neighbourhood to get started!</b>
+      </p>
+    )
   };
 
   getSelectedNeighbourhoods(selectedOptions) {
-    this.setState({
-      ...this.state,
-      selectedNeighbourhoods: selectedOptions
-    });
+    let selectedLength = selectedOptions ? selectedOptions.length : 0;
+    let selectionStatement;
+    switch (selectedLength) {
+      case 0:
+        selectionStatement = (
+          <p>
+            <b>Pick your neighbourhood to get started!</b>
+          </p>
+        );
+        break;
+      case 1:
+        selectionStatement = (
+          <p>
+            Select <b>one more</b> to start comparing!
+          </p>
+        );
+        break;
+      case 2:
+        selectionStatement = (
+          <p>
+            Isn't this <b>cool</b>? Try one more!
+          </p>
+        );
+        break;
+      case 3:
+        selectionStatement = (
+          <p>
+            You're the <b>best</b>!
+          </p>
+        );
+        break;
+      case 4:
+        selectionStatement = (
+          <p>Okay, we were just being nice. You need to slow down.</p>
+        );
+        break;
+      case 5:
+        selectionStatement = <p>Okay, now stop.</p>;
+        break;
+      case 6:
+        selectionStatement = <p>We don't do that here.</p>;
+        break;
+      default:
+        selectionStatement = <p>.. really?</p>;
+    }
+
+    // If selectedOptions go back to zero; set it to an empty Array else set it what it is.
+    if (selectedOptions != null)
+      this.setState({
+        ...this.state,
+        selectedNeighbourhoods: selectedOptions,
+        selectionStatement
+      });
+    else
+      this.setState({
+        ...this.state,
+        selectedNeighbourhoods: [],
+        selectionStatement
+      });
   }
   render() {
     return (
@@ -27,6 +87,20 @@ class Graphview extends Component {
           <p className="factParagraph">
             Get to know the city tree population by neighbourhoods.
           </p>
+
+          {/* {this.state.selectedNeighbourhoods.length == 1 ? (
+            <p>
+              Select <b>one more</b> to start comparing!
+            </p>
+          ) : this.state.selectedNeighbourhoods.length > 1 ? (
+            <p>Perfect!</p>
+          ) : (
+            <p>
+              <b>Pick your neighbourhood to get started!</b>
+            </p>
+          )} */}
+
+          {this.state.selectionStatement}
         </div>
         <div>
           <div className="graphviewContainer">
