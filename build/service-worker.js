713fe97a2f3c4ce7fea4146bf170e700caccdec1
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-12-07 00:35:26 -0800

    Latest Build -- 07/12 00:35 AM

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9a9ba3ef6ca905001e9e382bd199f28.js"
+  "/precache-manifest.415039c09a967b51c68822721802d945.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
