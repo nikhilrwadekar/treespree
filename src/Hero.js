d343b6aa674ca1dd50fbe0dffae5c186e252910e
jasminekaur95 <kjasmine2810@gmail.com>
2019-12-07 18:17:37 -0800

    added citations for my components

--- src/Hero.js
+++ src/Hero.js
@@ -21,9 +21,11 @@ function Hero() {
             height="100%"
             clip-path="url(#path)"
           >
+            {/* Code courtesy: Denis Billette */}
             <div className="bgVid-container">
               <video className="bgVid" autoPlay muted loop>
                 <source
+                  // REFERNCE (video from coverr.co: https://coverr.co/videos/A%20Beautiful%20Vermont%20Road--ea7c5966-8a6f-4b5b-88bb-579d69d465cb)
                   src="https://storage.googleapis.com/coverr-main/mp4%2Fcoverr-a-beautiful-vermont-road-1572327401402.mp4"
                   type="video/mp4"
                 />
